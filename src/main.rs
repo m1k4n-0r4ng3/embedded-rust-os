d4d7d5e7fbc17cf236dac5e77e20f0b37231da1c
m1k4n <>
2025-04-19 12:19:03 +0900

    impl systick interrupt

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,52 @@
 use core::panic::PanicInfo;
 use core::ptr;
 use cortex_m_semihosting::hprintln;
+mod systick;
+
+pub union Vector {
+    reserved: u32,
+    handler: unsafe extern "C" fn(),
+}
+
+extern "C" {
+    fn NMI();
+    fn HardFault();
+    fn MemManage();
+    fn BusFault();
+    fn UsageFault();
+    fn SVCall();
+    fn PendSV();
+    //fn SysTick();
+}
+
+#[link_section = ".vector_table.exceptions"]
+#[no_mangle]
+pub static EXCEPTIONS: [Vector; 14] = [
+    Vector { handler: NMI },
+    Vector { handler: HardFault },
+    Vector { handler: MemManage },
+    Vector { handler: BusFault },
+    Vector { handler: UsageFault },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { handler: SVCall },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { handler: PendSV },
+    Vector { handler: SysTick },
+];
+
+#[no_mangle]
+pub extern "C" fn DefaultExceptionHandler() {
+    loop{}
+}
+
+#[no_mangle]
+pub extern "C" fn SysTick() {
+    hprintln!("Systick interrupt");
+}
 
 // The reset vector, a pointer into the reset handler
 #[link_section = ".vector_table.reset_vector"]
@@ -25,9 +71,10 @@ pub unsafe extern "C" fn Reset() -> ! {
 
     let count = &raw const _edata as *const u8 as usize - &raw const _sdata as *const u8 as usize;
     ptr::copy_nonoverlapping(&raw mut _sidata as *const u8, &raw mut _sdata as *mut u8, count);
-    let _x = 42;
-    hprintln!("Hello World");
-    hprintln!("m1k4n");
+
+    hprintln!("Reset");
+
+    systick::init();
     
     loop {}
 }
